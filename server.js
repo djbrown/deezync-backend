10577c1972685acca4f52e35f37a2164594aa69f
Daniel Brown <djbrown@users.noreply.github.com>
2020-11-28 04:21:17 +0100

    implement server side authentication flow

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
+var http = require('https')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
 
+const APP_ID = process.env.APP_ID
+const SECRET_KEY = process.env.SECRET_KEY
+const REDIRECT_URI = process.env.REDIRECT_URI
+const FRONTEND_URL = process.env.FRONTEND_URL
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(APP_ID)
+  res.send(APP_ID)
 })
 
+app.get('/login', (req, res) => {
+  const loginUrl = `https://connect.deezer.com/oauth/auth.php?app_id=${APP_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&perms=basic_access,email`
+  res.redirect(loginUrl)
+})
+
+app.get('/deezer-login-callback', (req, res) => {
+  const authCode = encodeURIComponent(req.query.code)
+  const accessTokenUrl = `https://connect.deezer.com/oauth/access_token.php?app_id=${APP_ID}&secret=${SECRET_KEY}&code=${authCode}&output=json`
+  http.get(accessTokenUrl, (tokenRes) => {
+    let rawData = ''
+    tokenRes.on('data', (chunk) => {
+      rawData += chunk
+    })
+    tokenRes.on('end', () => {
+      const resJson = JSON.parse(rawData)
+      const accessToken = resJson.access_token
+      const expires = new Date(Date.now() + resJson.expires * 1000)
+      console.log(`${expires} `)
+      res.cookie('accessToken', accessToken, {
+          expires: expires,
+          secure: true
+        })
+        .redirect(FRONTEND_URL)
+    });
+  })
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`deezync-backend listening at http://localhost:${port}`)
 })
